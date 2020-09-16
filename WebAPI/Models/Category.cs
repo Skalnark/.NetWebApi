67fa470c4dd31675c553092cba0335022c185580
Skalnark <skalnark@live.com>
2020-09-15 22:39:30 -0300

    models and controllers added

---
+++ WebAPI/Models/Category.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string ImageUrl { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ WebAPI/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string ImageUrl { get; set; }
+        public float Amount { get; set; }
+        public DateTime Created { get; set; }
+
+        public Category Category { get; set; }
+
+        public int CategoryId { get; set; }
+
+    }
+}
